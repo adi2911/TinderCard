6155cad857524d11872a1ee4c2fdb33490dbec4e
adjohari <adjohari25@gmail.com>
2024-07-22 16:38:32 +0530

    one time password base

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
 import { StyleSheet, View } from "react-native";
-import Deck from "./src/components/Deck";
-import { DATA } from "./src/constant";
-import {
-  onSwipeLeft,
-  onSwipeRight,
-  renderCard,
-  renderNoMoreCards,
-} from "./src/helper";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Deck
-        data={DATA}
-        renderCard={renderCard}
-        onSwipeLeft={onSwipeLeft}
-        onSwipeRight={onSwipeRight}
-        renderNoMoreCards={renderNoMoreCards}
-      />
-    </View>
-  );
-}
+export default () => {
+  return <View style={styles.container}>App</View>;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -28,3 +10,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
 });
+
+// import Deck from "./src/components/Deck";
+// import { DATA } from "./src/constant";
+// import {
+//   onSwipeLeft,
+//   onSwipeRight,
+//   renderCard,
+//   renderNoMoreCards,
+// } from "./src/helper";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Deck
+//         data={DATA}
+//         renderCard={renderCard}
+//         onSwipeLeft={onSwipeLeft}
+//         onSwipeRight={onSwipeRight}
+//         renderNoMoreCards={renderNoMoreCards}
+//       />
+//     </View>
+//   );
+// }
---
+++ README.md
@@ -0,0 +1,4 @@
+### Saving code on firebase backend
+### Texting via sms to using Twilio
+### Sent a user JWT using firebase
+### Google Cloud Functions to run bit of code that run on demand to generate the OTP. (We can use AWS Lambda)
